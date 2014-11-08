2fc96c700aaba106beb0d49d5ce5f527a031521d
thaynsworth <thomas.haynsworth@gmail.com>
2014-11-08 15:26:03 -0500

    replaced game_state on page with dashes

--- controllers/hangman_games_controller.rb
+++ controllers/hangman_games_controller.rb
@@ -18,16 +18,37 @@ class HangmanGamesController < ApplicationController
 
     sample_word = ["goat", "sheep", "cow", "lamb"].sample
     user_id = current_user.id
-    game_state = []
+    game_state = sample_word.gsub(/\w/, '_')
     num_guess = 0
 
-    new_game = {
+    game = HangmanGame.create({word: sample_word, game_state: game_state})
+
+    {
       user_id: user_id,
       word: sample_word,
       game_state: game_state,
-      num_wrong_guesses: num_guess
+      num_wrong_guesses: num_guess,
+      gameid: game.id
     }.to_json
 
   end
+
+  patch '/:id/guess' do
+
+    game = HangmanGame.find(params[id])
+
+    state = game.game_state.dup
+
+    # modify the state if needed
+
+    game.game_state = state
+    game.save
+
+
+
+
+  end
+
+
 
 end
